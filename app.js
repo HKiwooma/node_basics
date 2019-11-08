7e7e319e4f9c1dc3b35932a1d8afe2fb47c9bec2
HKiwooma <har.mullah@gmail.com>
2019-11-08 16:40:52 +0300

    send respond post

--- app.js
+++ app.js
@@ -10,7 +10,14 @@ http.createServer(rqListener)
 
 // or use random function call event driven arctitechure 
 const server = http.createServer(function rqListener (req, res) {
-  console.log(req)
+  console.log(req.url, req.method, req.headers)
+  // process.exit() //to quit the server
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<html>') // help write small html
+  res.write('<head><title>My First Page</title></head>')
+  res.write('<body><h1>Hello from my node.js Serve!</h1></body>')
+  res.write('</body>')
 })
 
 server.listen(3000)
+// event listen never unregistered from
