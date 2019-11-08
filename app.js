845147802b851f4cc3555a03733f7252de81a820
HKiwooma <har.mullah@gmail.com>
2019-11-08 16:57:35 +0300

    adding routing requests

--- app.js
+++ app.js
@@ -12,14 +12,15 @@ http.createServer(rqListener)
 const server = http.createServer(function rqListener (req, res) {
   //   console.log(req.url, req.method, req.headers)
   // process.exit() //to quit the server
+  // routing requests
   const url = req.url
   if (url === '/') {
     res.setHeader('Content-Type', 'text/html')
     res.write('<html>') // help write small html
     res.write('<head><title>Enter Message</title></head>')
-    res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit"></button><form></body>')
+    res.write('<body><form action="/message" method="POST"><input type="text"><button type="submit">Send</button><form></body>')
     res.write('</body>')
-     return res.end()
+    return res.end()
   }
 
   res.setHeader('Content-Type', 'text/html')
